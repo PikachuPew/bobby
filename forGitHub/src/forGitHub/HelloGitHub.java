bfb7ee725b895d8ca99c6eaee770b7c184501623
PikachuPew <batbrendan000@gmail.com>
2025-09-24 09:44:34 -0500

    bro why isnt this working

--- forGitHub/src/forGitHub/HelloGitHub.java
+++ forGitHub/src/forGitHub/HelloGitHub.java
@@ -4,16 +4,12 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello again");
-<<<<<<< HEAD
-		//idk it just felt right
+		//idk it just felt right, or does it? (V sauce music starts playing). Say hello, developer #2.
 		
 		//hello my name is markiplier and I am a fish
 		for(int i = 0; i < 5; i++) {
 			System.out.println("fish.");
 		}
-=======
-		//idk it just felt right, or does it? (V sauce music starts playing). Say hello, developer #2.
->>>>>>> branch 'main' of https://github.com/PikachuPew/bobby.git
 	}
 
 }
