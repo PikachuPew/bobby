7aa31b51d885c8265b8e793e2dd58d566bd41067
PikachuPew <batbrendan000@gmail.com>
2025-09-24 09:34:23 -0500

    added a for loop

--- forGitHub/src/forGitHub/HelloGitHub.java
+++ forGitHub/src/forGitHub/HelloGitHub.java
@@ -5,6 +5,11 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("Hello again");
 		//idk it just felt right
+		
+		//hello my name is markiplier and I am a fish
+		for(int i = 0; i < 5; i++) {
+			System.out.println("fish.");
+		}
 	}
 
 }
