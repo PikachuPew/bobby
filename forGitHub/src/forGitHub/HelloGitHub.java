f2a027154cf5f2ad240489c68118e02820acaa92
PikachuPew <batbrendan000@gmail.com>
2025-09-24 09:31:51 -0500

    added to my comment

--- forGitHub/src/forGitHub/HelloGitHub.java
+++ forGitHub/src/forGitHub/HelloGitHub.java
@@ -4,7 +4,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello again");
-		//idk it just felt right
+		//idk it just felt right, or does it? (V sauce music starts playing). Say hello, developer #2.
 	}
 
 }
